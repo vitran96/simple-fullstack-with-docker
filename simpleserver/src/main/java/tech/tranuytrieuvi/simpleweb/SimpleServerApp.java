ecd78953d937ba9230a425c89287a170dc167998
vitran96 <tranuytrieuvi@gmail.com>
2024-01-31 22:29:11 +0700

    feature(server) - setup simple server

--- simpleserver/src/main/java/tech/tranuytrieuvi/simpleweb/SimpleServerApp.java
+++ simpleserver/src/main/java/tech/tranuytrieuvi/simpleweb/SimpleServerApp.java
@@ -1,5 +1,6 @@
 package tech.tranuytrieuvi.simpleweb;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
@@ -9,30 +10,57 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
+import java.sql.*;
 import java.time.Duration;
 import java.util.Arrays;
 
 @SpringBootApplication
-//@EnableConfigurationProperties
-//@EntityScan(basePackages = {"tech.tranuytrieu.simpleweb.model"})
+@Slf4j
 public class SimpleServerApp {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SimpleServerApp.class, args);
-	}
+    private static void setup() {
+        log.debug("Creating database if not exist...");
+        try (Connection connection = DriverManager.getConnection(
+                "jdbc:postgresql://localhost:5432/",
+                "postgres",
+                "Root@2023"
+        );
+             Statement statement = connection.createStatement()
+        ) {
+            String databaseName = "bookstore";
+            // Check if database exists
+            ResultSet resultSet = statement.executeQuery("SELECT datname FROM pg_catalog.pg_database WHERE datname = '" + databaseName + "'");
+            if (resultSet.next()) {
+                log.debug("Database already exists");
+                return;
+            }
 
-	@Bean
-	CorsConfigurationSource corsConfigurationSource() {
-		CorsConfiguration configuration = new CorsConfiguration();
 
-		configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000"));
-		configuration.setAllowedMethods(Arrays.asList("GET","POST","PUT","DELETE", "OPTIONS"));
-		configuration.setAllowedHeaders(Arrays.asList("Content-Type", "Authorization"));
-		configuration.setMaxAge(Duration.ofMinutes(5));
+            statement.executeUpdate("CREATE DATABASE " + databaseName);
+            log.debug("Database created successfully");
+        } catch (SQLException e) {
+            log.error("Error", e);
+            throw new RuntimeException(e);
+        }
+    }
 
-		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-		source.registerCorsConfiguration("/**", configuration);
-		return source;
-	}
+    public static void main(String[] args) {
+        setup();
+        SpringApplication.run(SimpleServerApp.class, args);
+    }
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000"));
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        configuration.setAllowedHeaders(Arrays.asList("Content-Type", "Authorization"));
+        configuration.setMaxAge(Duration.ofMinutes(5));
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
 
 }
