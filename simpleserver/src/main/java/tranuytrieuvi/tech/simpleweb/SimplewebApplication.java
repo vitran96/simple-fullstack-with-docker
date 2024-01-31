83db150df83a7c1601024a2e99f70d18ca4b1b5f
vitran96 <tranuytrieuvi@gmail.com>
2024-01-31 21:20:51 +0700

    feature(server) - add server

---
+++ simpleserver/src/main/java/tranuytrieuvi/tech/simpleweb/SimplewebApplication.java
@@ -0,0 +1,13 @@
+package tranuytrieuvi.tech.simpleweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimplewebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimplewebApplication.class, args);
+	}
+
+}
---
+++ simpleserver/src/test/java/tranuytrieuvi/tech/simpleweb/SimplewebApplicationTests.java
@@ -0,0 +1,13 @@
+package tranuytrieuvi.tech.simpleweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimplewebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
