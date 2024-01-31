01447d3bbf411549da1afca2a311a0d4abdd579b
vitran96 <tranuytrieuvi@gmail.com>
2024-02-01 01:01:06 +0700

    feature(server) - add delete route

--- simpleserver/src/main/java/tech/tranuytrieuvi/simpleweb/controller/BookController.java
+++ simpleserver/src/main/java/tech/tranuytrieuvi/simpleweb/controller/BookController.java
@@ -17,35 +17,41 @@ import java.util.UUID;
 @RequestMapping("/api/v1")
 public class BookController {
 
-    @Autowired
-    private final BookRepo bookRepo;
+  @Autowired
+  private final BookRepo bookRepo;
 
-    @GetMapping("/books")
-    public List<Book> getAllBooks() {
-        return bookRepo.findAll();
-    }
+  @GetMapping("/books")
+  public List<Book> getAllBooks() {
+    return bookRepo.findAll();
+  }
 
-    @GetMapping("/book/{id}")
-    public Book getBookById(@PathVariable(value = "id") UUID bookId) throws ResourceNotFoundException {
-        return bookRepo.findById(bookId)
-                .orElseThrow(() -> new ResourceNotFoundException("Book not found for this id :: " + bookId));
-    }
+  @GetMapping("/book/{id}")
+  public Book getBookById(@PathVariable(value = "id") UUID bookId) throws ResourceNotFoundException {
+    return bookRepo.findById(bookId)
+        .orElseThrow(() -> new ResourceNotFoundException("Book not found for this id :: " + bookId));
+  }
 
-    @PostMapping("/book")
-    public Book createBook(@RequestBody Book book) {
-        return bookRepo.save(book);
-    }
+  @PostMapping("/book")
+  public Book createBook(@RequestBody Book book) {
+    return bookRepo.save(book);
+  }
 
-    @PutMapping("/book/{id}")
-    public Book updateBook(
-            @PathVariable(value = "id") UUID bookId,
-            @RequestBody Book bookDetails
-    ) throws ResourceNotFoundException {
-        Book book = bookRepo.findById(bookId)
-                .orElseThrow(() -> new ResourceNotFoundException("Book not found for this id :: " + bookId));
-        book.setTitle(bookDetails.getTitle());
-        book.setAuthor(bookDetails.getAuthor());
-        book.setDescription(bookDetails.getDescription());
-        return bookRepo.save(book);
-    }
+  @PutMapping("/book/{id}")
+  public Book updateBook(
+      @PathVariable(value = "id") UUID bookId,
+      @RequestBody Book bookDetails) throws ResourceNotFoundException {
+    Book book = bookRepo.findById(bookId)
+        .orElseThrow(() -> new ResourceNotFoundException("Book not found for this id :: " + bookId));
+    book.setTitle(bookDetails.getTitle());
+    book.setAuthor(bookDetails.getAuthor());
+    book.setDescription(bookDetails.getDescription());
+    return bookRepo.save(book);
+  }
+
+  @DeleteMapping("/book/{id}")
+  public void deleteBook(@PathVariable(value = "id") UUID bookId) throws ResourceNotFoundException {
+    Book book = bookRepo.findById(bookId)
+        .orElseThrow(() -> new ResourceNotFoundException("Book not found for this id :: " + bookId));
+    bookRepo.delete(book);
+  }
 }
