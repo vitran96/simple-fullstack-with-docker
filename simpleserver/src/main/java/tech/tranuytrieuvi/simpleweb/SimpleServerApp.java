8813d3b90411d322a6fcb22b895165ce2a46ba4a
vitran96 <tranuytrieuvi@gmail.com>
2024-02-01 13:38:49 +0700

    refactor

--- simpleserver/src/main/java/tech/tranuytrieuvi/simpleweb/SimpleServerApp.java
+++ simpleserver/src/main/java/tech/tranuytrieuvi/simpleweb/SimpleServerApp.java
@@ -18,50 +18,51 @@ import java.util.Arrays;
 @Slf4j
 public class SimpleServerApp {
 
-    private static void setup() {
-        log.debug("Creating database if not exist...");
-        try (Connection connection = DriverManager.getConnection(
-                "jdbc:postgresql://localhost:5432/",
-                "postgres",
-                "Root@2023"
-        );
-             Statement statement = connection.createStatement()
-        ) {
-            String databaseName = "bookstore";
-            // Check if database exists
-            ResultSet resultSet = statement.executeQuery("SELECT datname FROM pg_catalog.pg_database WHERE datname = '" + databaseName + "'");
-            if (resultSet.next()) {
-                log.debug("Database already exists");
-                return;
-            }
+  private static void setup() {
+    log.debug("Creating database if not exist...");
+    try (Connection connection = DriverManager.getConnection(
+        "jdbc:postgresql://localhost:5432/",
+        "postgres",
+        "Root@2023");
+        Statement statement = connection.createStatement()) {
+      String databaseName = "bookstore";
+      // Check if database exists
+      ResultSet resultSet = statement
+          .executeQuery("SELECT datname FROM pg_catalog.pg_database WHERE datname = '" + databaseName + "'");
+      if (resultSet.next()) {
+        log.debug("Database already exists");
+        return;
+      }
 
-
-            statement.executeUpdate("CREATE DATABASE " + databaseName);
-            log.debug("Database created successfully");
-        } catch (SQLException e) {
-            log.error("Error", e);
-            throw new RuntimeException(e);
-        }
+      statement.executeUpdate("CREATE DATABASE " + databaseName);
+      log.debug("Database created successfully");
+    } catch (SQLException e) {
+      log.error("Error", e);
+      throw new RuntimeException(e);
     }
+  }
 
-    public static void main(String[] args) {
-        setup();
-        SpringApplication.run(SimpleServerApp.class, args);
-    }
+  public static void main(String[] args) {
+    // setup();
+    SpringApplication.run(SimpleServerApp.class, args);
+  }
 
-//    @Bean
-//    CorsConfigurationSource corsConfigurationSource() {
-//        CorsConfiguration configuration = new CorsConfiguration();
-//
-////        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000"));
-//        configuration.setAllowedOrigins(Arrays.asList("*"));
-//        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
-//        configuration.setAllowedHeaders(Arrays.asList("Content-Type", "Authorization"));
-//        configuration.setMaxAge(Duration.ofMinutes(5));
-//
-//        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-//        source.registerCorsConfiguration("/**", configuration);
-//        return source;
-//    }
+  // @Bean
+  // CorsConfigurationSource corsConfigurationSource() {
+  // CorsConfiguration configuration = new CorsConfiguration();
+  //
+  //// configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000"));
+  // configuration.setAllowedOrigins(Arrays.asList("*"));
+  // configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE",
+  // "OPTIONS"));
+  // configuration.setAllowedHeaders(Arrays.asList("Content-Type",
+  // "Authorization"));
+  // configuration.setMaxAge(Duration.ofMinutes(5));
+  //
+  // UrlBasedCorsConfigurationSource source = new
+  // UrlBasedCorsConfigurationSource();
+  // source.registerCorsConfiguration("/**", configuration);
+  // return source;
+  // }
 
 }
